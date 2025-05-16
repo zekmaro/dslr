8295104e9f481b404da35774e7c210d5377db2c5
Andrey Arama <zekmaro@yandex.ru>
2025-05-16 12:35:24 +0200

    refact: seperate plotting

--- src/scatter_plot.py
+++ src/scatter_plot.py
@@ -3,15 +3,27 @@ import matplotlib.pyplot as plt
 from typing import List
 
 
-def find_similar_features(df: pd.DataFrame):
-	"""Find similar features in a DataFrame based on mean and standard deviation."""
-	num_columns = [col for col in df.columns if df[col].dtype in [int, float]]
+def plot_features(data: pd.DataFrame, feature_1: pd.DataFrame, feature_2: pd.DataFrame):
 	house_colors = {
 		"Gryffindor": "red",
 		"Slytherin": "green",
 		"Ravenclaw": "blue",
 		"Hufflepuff": "orange"
 	}
+	for house, color in house_colors.items():
+		subset = data[data["Hogwarts House"] == house]
+		plt.scatter(subset[feature_1], subset[feature_2], label=house, color=color, alpha=0.6, s=15)
+	plt.xlabel(feature_1)
+	plt.ylabel(feature_2)
+	plt.title(f"{feature_1} vs {feature_2}")
+	plt.legend()
+	plt.grid(True)
+	plt.show()
+
+
+def find_similar_features(df: pd.DataFrame):
+	"""Find similar features in a DataFrame based on mean and standard deviation."""
+	num_columns = [col for col in df.columns if df[col].dtype in [int, float]]
 	col_data = {
 		col: {
 			"mean": df[col].mean(),
@@ -22,15 +34,7 @@ def find_similar_features(df: pd.DataFrame):
 	for col1, data1 in col_data.items():
 		for col2, data2 in col_data.items():
 			if col1 != col2 and abs(data1["mean"] - data2["mean"]) < 1 and abs(data1["std"] - data2["std"]) < 1:
-				if not (col1, col2) in similar_features or not (col2, col1) in similar_features:
+				if not (col1, col2) in similar_features and not (col2, col1) in similar_features:
 					similar_features.append((col1, col2))
-					for house, color in house_colors.items():
-						subset = df[df["Hogwarts House"] == house]
-						plt.scatter(subset[col1], subset[col2], label=house, color=color, alpha=0.6, s=15)
-					plt.xlabel(col1)
-					plt.ylabel(col2)
-					plt.title(f"{col1} vs {col2}")
-					plt.legend()
-					plt.grid(True)
-					plt.show()
+					plot_features(df, col1, col2)
 	return similar_features
