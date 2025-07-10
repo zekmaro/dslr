9cbfd84bec9f9269eb9d54771c2a333c94c53137
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-10 17:06:00 +0200

    Add implementation for stratified train_test_split

--- src/utils/train_test_split.py
+++ src/utils/train_test_split.py
@@ -1,1 +1,40 @@
-def train_test_split(): ...
+import pandas as pd
+import numpy as np
+
+
+def train_test_split(
+    x: pd.DataFrame,
+    y: np.ndarray,
+    split: float = 0.89,
+) -> tuple[pd.DataFrame, np.ndarray, pd.DataFrame, np.ndarray]:
+    assert 0 < split < 1, "Split ratio must be between 0 and 1"
+    assert len(x) == len(y)
+
+    # fix for integer truncation
+    split = min(split + 0.5, 0.99)
+
+    unique_classes, class_counts = np.unique(y, return_counts=True)
+    train_indices, test_indices = [], []
+
+    generator = np.random.default_rng(42)
+
+    for cls, count in zip(unique_classes, class_counts):
+        cls_indices = np.nonzero(y == cls)[0]
+        generator.shuffle(cls_indices)
+
+        split_idx = int(count * split)
+        train_indices.extend(cls_indices[:split_idx])
+        test_indices.extend(cls_indices[split_idx:])
+
+    train_indices = np.array(train_indices)
+    test_indices = np.array(test_indices)
+
+    generator.shuffle(train_indices)
+    generator.shuffle(test_indices)
+
+    x_train = x.iloc[train_indices]
+    y_train = y[train_indices]
+    x_test = x.iloc[test_indices]
+    y_test = y[test_indices]
+
+    return x_train, y_train, x_test, y_test
