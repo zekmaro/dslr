f734726e4e9f4b183b58e5d8c33f6fd4a1eee943
Vladimir Vilenskii <vilenskii.v@gmail.com>
2026-06-20 14:06:42 +0200

    bd anal

--- src/cheats/cheat.py
+++ src/cheats/cheat.py
@@ -4,7 +4,6 @@ whole thing implemented with scikit
 
 # import sklearn
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -17,7 +16,7 @@ SURNAME_DIST_PATH = "./images/surname_distribution.png"
 
 def get_correlations(df: pd.DataFrame):
     corr_matrix = df.corr(numeric_only=True)
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(15, 10))
     cmap = sns.diverging_palette(230, 20, as_cmap=True)
     sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap=cmap, center=0,
                 square=True, linewidths=.5, cbar_kws={"shrink": .5})
@@ -58,6 +57,13 @@ def get_name_distribution(df: pd.DataFrame):
     plt.savefig(SURNAME_DIST_PATH, dpi=150, bbox_inches='tight')
     plt.close()
 
+def add_bd_cols(df: pd.DataFrame):
+    bd_col = pd.to_datetime(df['Birthday'])
+    df['BD day'] = bd_col.dt.day
+    df['BD month'] = bd_col.dt.month
+    df['BD year'] = bd_col.dt.year
+    df['BD doy'] = bd_col.dt.dayofyear
+    df['BD weekday'] = bd_col.dt.weekday
 
 def prepare_dataset(path):
     train_data = pd.read_csv(path)
@@ -66,14 +72,74 @@ def prepare_dataset(path):
     hand_mapping = {k: v for v, k in enumerate(train_data['Best Hand'].unique())}
     train_data['Best Hand'] = train_data['Best Hand'].map(hand_mapping)
 
-    get_name_distribution(train_data)
-    # get_correlations(train_data)
+    # get_name_distribution(train_data)
+    add_bd_cols(train_data)
+    get_correlations(train_data)
+
+    # train_data = train_data.drop("Defense Against the Dark Arts")  # is correlated with Astronomy
+
     return train_data
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.datasets import make_classification
+from sklearn.datasets import load_iris
 
 def main() -> None:
     """Load the trained model and evaluate its accuracy on the test set."""
     df = prepare_dataset(DATA_PATH)
+
+    y = df['Hogwarts House']
+    # X = df[['Best Hand', '']]
+
+
+# ---------------------------------------------------
+# Train / Test Split
+# ---------------------------------------------------
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X,
+#     y,
+#     test_size=0.2,
+#     random_state=42,
+#     stratify=y
+# )
+
+# # ---------------------------------------------------
+# # Pipeline
+# # ---------------------------------------------------
+# pipeline = Pipeline([
+#     ("scaler", StandardScaler()),
+#     (
+#         "classifier",
+#         OneVsRestClassifier(
+#             LogisticRegression(
+#                 max_iter=5000,
+#                 solver="lbfgs"
+#             )
+#         )
+#     )
+# ])
+
+# # ---------------------------------------------------
+# # Train
+# # ---------------------------------------------------
+# pipeline.fit(X_train, y_train)
+
+# # ---------------------------------------------------
+# # Predict
+# # ---------------------------------------------------
+# y_pred = pipeline.predict(X_test)
+
+# # ---------------------------------------------------
+# # Evaluation
+# # ---------------------------------------------------
+# print("Accuracy:", accuracy_score(y_test, y_pred))
+# print("\nClassification Report:\n")
+# print(classification_report(y_test, y_pred))
 
 
 if __name__ == "__main__":
